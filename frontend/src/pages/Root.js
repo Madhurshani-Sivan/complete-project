6c47cb40ec43ec6a612410a3528b91c6ee76cc44
madhurshani-sivan <madhurshanisivan@gmail.com>
2023-08-08 17:09:06 +0530

    remove timeout

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,14 +1,17 @@
-import { Outlet, useLoaderData } from "react-router-dom";
+import { Outlet, useLoaderData, useNavigation } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
 
 const RootLayout = () => {
   /* const events = useLoaderData();
   console.log(events); */ //CANT USE LOADER DATA IN UPPER LEVEL PATHS - UNDEFINED
 
+  const navigation = useNavigation();
+
   return (
     <>
       <MainNavigation />
       <main>
+        {/* {navigation.state === "loading" && <p>Loading...</p>} */}
         <Outlet />
       </main>
     </>
