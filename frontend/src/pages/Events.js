f371ede3e41fa1435b43e06a51c4427a30834121
madhurshani-sivan <madhurshanisivan@gmail.com>
2023-08-09 13:17:03 +0530

    handle loading using defer

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,21 +1,32 @@
-import { json, useLoaderData } from "react-router-dom";
+import { Await, defer, json, useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
+import { Suspense } from "react";
 
 function EventsPage() {
-  const data = useLoaderData();
+  const { events } = useLoaderData();
 
-  /*  if (data.isError) {
-    return <p>{data.message}</p>;
-  } */
-
-  const events = data.events;
-
-  return <EventsList events={events} />;
+  return (
+    <Suspense
+      fallback={
+        <p
+          style={{
+            textAlign: "center",
+          }}
+        >
+          Loading...{" "}
+        </p>
+      }
+    >
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
+  );
 }
 
 export default EventsPage;
 
-export async function eventsLoader() {
+async function loadEvents() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
@@ -42,6 +53,13 @@ export async function eventsLoader() {
       }
     );
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.events;
   }
+}
+
+export function eventsLoader() {
+  return defer({
+    events: loadEvents(),
+  });
 }
